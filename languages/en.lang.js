5e50be810ab30ca158f15780303d4d7c78bb5f44
mirrykal3 <arun.kumar.x65@gmail.com>
2025-05-17 23:20:08 +0530

    Create en.lang.js

---
+++ languages/en.lang.js
@@ -0,0 +1,7 @@
+module.exports = {
+  unknownCommand: "Umm... '%1'? Yeh kya tha sweetie? Mujhe toh samajh hi nahi aaya!",
+  helpHint: "Agar confuse ho gaye ho toh /help type karo na, main bata dungi!",
+  commandError: "Oh nooo! Kuch toh galat ho gaya... thoda der baad try kar lena please?",
+  startMessage: (name, botname, prefix) =>
+    `Hii ${name}!\nWelcome to *${botname}* 🥳🥳🥳!\nAgar kuch chahiye toh just type \`${prefix}help\`, okayy?`,
+};
